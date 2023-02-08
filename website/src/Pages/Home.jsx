edae5ca714a77b78447ecb26dcef442748b72fee
maaz khan <maazkhaansofteng@gmail.com>
2023-02-08 18:15:56 +0500

    some modification

--- website/src/Pages/Home.jsx
+++ website/src/Pages/Home.jsx
@@ -70,10 +70,9 @@ const Home = () => {
               spot.
             </p>
             <div class="DownloadButton">
-                   <button class="button">Download</button>
-              </div>
+              <button class="button">Download</button>
+            </div>
           </div>
-  
         </div>
         <div className="right">
           <div class="mobile">
@@ -89,10 +88,72 @@ const Home = () => {
                   height="535"
                   width="312"
                 />
-               
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="SpotSense">
+        <div className="left">
+          <div className="para">
+            <h1>SpotSense</h1>
+            <p>
+              Using advanced sensors and algorithms, SpotSense can ​detect the
+              presence of cars in real-time and provide accurate information
+              about the availability of parking spaces.
+            </p>
+            <p>
+              If you are a parking lot owner, we have a plan for you to use
+              ​these smart parking sensors on your parking lot. Get in in ​touch
+              with us using the button below.
+            </p>
+            <div class="DownloadButton">
+              <button class="button">Order now</button>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <img
+            src="../src/assets/Parking.webp"
+            alt="Map picture"
+            style={{ objectFit: "cover", width: "534px", height: "621px" }}
+            className="content"
+          />
+          <img
+            src="../src/assets/SensorImage.webp"
+            alt="Sensor"
+            width={93.49}
+            height={30.39}
+            className="sensor1"
+          />
+        </div>
+      </div>
+
+      <div className="SpotSense">
+        <div className="left">
+          <div className="para">
+            <h1>OverWatch ​Analytics</h1>
+            <p>
+              With OverWatch Data Analytics platform, you can ​have an overview
+              of how your parking spaces are ​being used.
+            </p>
+            <p>
+              We also plan to use the power of AI to provide ​suggestions and
+              warnings for traffic management ​purposes.
+            </p>
+            <div class="DownloadButton">
+              <button class="button">Order now</button>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <img
+            src="../src/assets/SearchingImage.webp"
+            alt="​Analytics picture"
+            style={{  width: "688px", height: "359px" }}
+            className="content"
+          />
+        
         </div>
       </div>
     </>
